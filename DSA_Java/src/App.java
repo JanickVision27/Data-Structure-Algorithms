6b4b60d3c9308ab031a1cee08af40347648b6c00
Venkata Vikranth Jannatha <vjannatha@gmail.com>
2026-05-30 11:04:50 +0530

    push

--- DSA_Java/src/App.java
+++ DSA_Java/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Pattern_based patterns = new Pattern_based();
 
-        patterns.Pattern2();
+        patterns.pattern4();
     }
     
 }
--- DSA_Java/src/Pattern_based.java
+++ DSA_Java/src/Pattern_based.java
@@ -16,7 +16,7 @@ public class Pattern_based {
         int n = 6;
         System.out.println();
 
-        for(int i =0; i< n; i++){
+        for(int i =0; i < n; i++){
             
             for (int j = 0; j<= i; j++){
                 System.out.print("*");
@@ -27,6 +27,35 @@ public class Pattern_based {
         }
 
     }
+
+    public void Pattern3() 
+    {
+        System.out.println("Please Provide a Number");
+        int n = sc.nextInt();
+
+        System.out.println();
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void pattern4()
+    {
+        for(int i = 1; i < 6; i++){
+            System.out.println(i);
+            for (int j = i; j <= i; j++){
+                System.out.print(j);
+            }
+        }
+        
+    }
+
 
 
 }
