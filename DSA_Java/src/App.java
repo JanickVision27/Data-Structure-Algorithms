f038b91af52c5473273bacd5e3cffb600bc7e2df
Venkata Vikranth Jannatha <vjannatha@gmail.com>
2026-06-09 17:28:28 +0530

    push

--- DSA_Java/src/App.java
+++ DSA_Java/src/App.java
@@ -2,9 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Pattern_based patterns = new Pattern_based();
         // Basic_Math math = new Basic_Math();
-        Basic_recursion rec = new Basic_recursion();
+        // Basic_recursion rec = new Basic_recursion();
+        Basic_Hashing hash = new Basic_Hashing();
+        hash.countFreq();
         
-        rec.isPalindrome("A man, a plan, a canal: Panama");
 
 
     }
---
+++ DSA_Java/src/Basic_Hashing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Basic_Hashing {
+
+    public void countFreq() {
+        int[] arr = { 10, 5, 10, 15, 10, 5 };
+        int n = arr.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i],
+                    map.getOrDefault(arr[i], 0) + 1);
+
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+    }
+}
