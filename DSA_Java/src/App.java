ff846d3158cfd5fc892825e13e08de4478ba32df
Venkata Vikranth Jannatha <vjannatha@gmail.com>
2026-06-17 10:12:49 +0530

    push

--- DSA_Java/src/App.java
+++ DSA_Java/src/App.java
@@ -3,9 +3,10 @@ public class App {
         // Pattern_based patterns = new Pattern_based();
         // Basic_Math math = new Basic_Math();
         // Basic_recursion rec = new Basic_recursion();
-        Basic_Hashing hash = new Basic_Hashing();
-        // hash.findHighest_Lowest();
-        hash.Testing_Hashing();
+        // Basic_Hashing hash = new Basic_Hashing();
+        Arrays_Easy ar1 = new Arrays_Easy();
+        // ar1.LargestElement();
+        ar1.get_second_min_max();
         
 
 
--- DSA_Java/src/Arrays_Easy.java
+++ DSA_Java/src/Arrays_Easy.java
@@ -1,3 +1,62 @@
+import java.util.*;
+
 public class Arrays_Easy {
-    
+
+    /*
+     * Basically use the first element as a Largest element
+     */
+    public void LargestElement() {
+        int[] arr = { 8, 10, 5, 7, 9 };
+        int MaxVal = arr[0];
+
+        if (arr.length == 0) {
+            System.out.println(-1);
+        }
+
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > MaxVal) {
+                MaxVal = arr[i];
+            }
+        }
+
+        System.out.println("Maximum Value : " + MaxVal);
+
+    }
+
+    public void get_second_min_max() {
+        int[] arr = { 1, 2, 4, 7, 7, 5 };
+
+        int maxVal = arr[0];
+        int minVal = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            } else if (arr[i] < minVal) {
+                minVal = arr[i];
+            }
+        }
+
+        // USe max for determining min and use min for determining Max values
+        int secondMax = Integer.MIN_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        System.out.println();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > secondMax && arr[i] != maxVal) {
+                secondMax = arr[i];
+            }
+
+            if (arr[i] < secondMin && arr[i] != minVal) {
+                secondMin = arr[i];
+            }
+        }
+
+        System.out.println("Second Highest: " + secondMax);
+        System.out.println("Second Smallest : " + secondMin);
+
+    }
+
 }
