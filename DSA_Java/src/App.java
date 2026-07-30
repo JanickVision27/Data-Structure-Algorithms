7983c07315c09e474ddeb4eba68b94a4ac66013f
Venkata Vikranth Jannatha <vjannatha@gmail.com>
2026-07-30 10:08:41 +0530

    push

--- DSA_Java/src/App.java
+++ DSA_Java/src/App.java
@@ -1,20 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // Pattern_based patterns = new Pattern_based();
-        // Basic_Math math = new Basic_Math();
-        // Basic_recursion rec = new Basic_recursion();
-        // Basic_Hashing hash = new Basic_Hashing();
-        Arrays_Easy ar1 = new Arrays_Easy();
-        // int[] myNumbers = { 1,0,1,1,0,1 };
-        // // 3. Pass the array into the parameter of your method
-        // int result = ar1.MaxConsecutiveOnes(myNumbers);
+        Basic_recursion r = new Basic_recursion();
+        r.checkSubArr();
 
-        // // 4. Print the result that was returned
-        // System.out.println("The maximum consecutive ones is: " + result);
-        // ar1.LongestSubArray();
-
-        Arrays_Medium ar2 = new Arrays_Medium();
-        ar2.setZeroMatrix(new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}});
     }
 
 }
--- DSA_Java/src/Basic_recursion.java
+++ DSA_Java/src/Basic_recursion.java
@@ -1,11 +1,12 @@
 import java.util.*;
+
 public class Basic_recursion {
 
-    public void reverseArray(int [] arr){
+    public void reverseArray(int[] arr) {
         int l = 0;
         int r = arr.length - 1;
 
-        while (l < r){
+        while (l < r) {
             int temp = arr[l];
             arr[l] = arr[r];
             arr[r] = temp;
@@ -15,23 +16,21 @@ public class Basic_recursion {
         }
 
     }
-    
+
     public boolean isPalindrome(String s) {
         int l = 0;
         int r = s.length() - 1;
 
-        while (l < r){
+        while (l < r) {
             char leftchar = s.charAt(l);
             char rightchar = s.charAt(r);
 
-            if(!Character.isLetterOrDigit(leftchar)){
+            if (!Character.isLetterOrDigit(leftchar)) {
                 l++;
-            }
-            else if(!Character.isLetterOrDigit(rightchar)){
+            } else if (!Character.isLetterOrDigit(rightchar)) {
                 r--;
-            }
-            else{
-                if(Character.toLowerCase(leftchar) != Character.toLowerCase(rightchar)){
+            } else {
+                if (Character.toLowerCase(leftchar) != Character.toLowerCase(rightchar)) {
                     System.out.println("False");
                     return false;
                 }
@@ -39,10 +38,33 @@ public class Basic_recursion {
                 r--;
             }
 
-
         }
         System.out.println("True");
         return true;
-        
+
+    }
+
+    public void checkSubArr() {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        int sum = 9;
+        int runningsum = 0;
+        int count = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1);
+        for (int i = 0; i < n; i++) {
+            runningsum += arr[i];
+            int val = runningsum - sum;
+
+            if (map.containsKey(val)) {
+                count += map.get(val);
+            }
+
+            map.put(runningsum, map.getOrDefault(runningsum, 0) + 1);
+
+        }
+
     }
 }
