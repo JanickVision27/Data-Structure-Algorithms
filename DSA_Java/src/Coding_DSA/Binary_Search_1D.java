920c05ff17904024d25f97a90314f9f40c1af016
JanickVision27 <vickyvenkata295@gmail.com>
2026-09-05 17:52:25 +0530

    Update Binary_Search_1D.java

--- DSA_Java/src/Coding_DSA/Binary_Search_1D.java
+++ DSA_Java/src/Coding_DSA/Binary_Search_1D.java
@@ -23,7 +23,7 @@ public class Binary_Search_1D {
         return -1;
     }
 
-    static int lowerBound(int[] nums, int x) {
+    public static int lowerBound(int[] nums, int x) {
         int left = 0;
         int right = nums.length - 1;
         int len = nums.length;
@@ -36,13 +36,13 @@ public class Binary_Search_1D {
             } else {
                 left = mid + 1;
             }
-        }
 
+        }
         System.out.println(len);
         return len;
     }
 
-    static int upperBound(int[] nums, int x) {
+    public static int upperBound(int[] nums, int x) {
         int left = 0;
         int right = nums.length - 1;
         int len = nums.length;
@@ -81,30 +81,13 @@ public class Binary_Search_1D {
     }
 
     static void findFloor(int[] nums, int tar) {
-
-        // Floor
+        // celing code
         int left = 0;
         int right = nums.length - 1;
-        int flor = 0;
-        int celing = 0;
+        int celing = -1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            if (nums[mid] <= tar) {
-                flor = nums[mid];
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        left = 0;
-        right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
             if (nums[mid] >= tar) {
                 celing = nums[mid];
                 right = mid - 1;
@@ -113,9 +96,108 @@ public class Binary_Search_1D {
             }
         }
 
-        System.out.println("Floor : " + flor);
-        System.out.println("Celing: " + celing);
+        int left_floor = 0;
+        int right_floor = nums.length - 1;
+        int floor = -1;
 
+        while (left_floor <= right_floor) {
+            int mid = left_floor + (right_floor - left_floor) / 2;
+
+            if (nums[mid] <= tar) {
+                floor = nums[mid];
+                left_floor = mid + 1;
+            } else if (nums[mid] > tar) {
+                right_floor = mid - 1;
+            }
+        }
+
+        System.out.println("Celing is :" + celing);
+        System.out.println("Floor is: " + floor);
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+
+        if (nums.length == 0)
+            return new int[] { -1, -1 };
+
+        int left = 0;
+        int right = nums.length - 1;
+        int start = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] >= target) {
+                start = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        if (start == nums.length || nums[start] != target) {
+            return new int[] { -1, -1 };
+        }
+
+        int left_end = 0;
+        int right_end = nums.length - 1;
+        int upperBound = nums.length;
+
+        while (left_end <= right_end) {
+            int mid = left_end + (right_end - left_end) / 2;
+
+            if (nums[mid] > target) {
+                upperBound = mid;
+                right_end = mid - 1;
+            } else {
+                left_end = mid + 1;
+            }
+        }
+
+        int last = upperBound - 1;
+
+        return new int[] { start, last };
+    }
+
+    public static int TotalOccurance(int[] arr, int k) {
+        int left = 0;
+        int right = arr.length - 1;
+        int start = arr.length;
+
+        if(arr.length == 0){
+            return 0;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= k) {
+                start = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        if(start == arr.length || arr[start] != k){
+            return 0;
+        }
+
+        left = 0;
+        right = arr.length - 1;
+        int end = arr.length;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > k) {
+                end = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        int totalOcc = end - start;
+
+        return totalOcc;
     }
 
     public static void main(String[] args) {
